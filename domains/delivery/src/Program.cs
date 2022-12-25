7a02740a16386c1d1ad5a9a9cf1ed87dc39dc7a2
James Wilson <jw@jameswilson.io>
2022-12-25 21:41:58 +1100

    adopted snake case package

--- domains/delivery/src/Program.cs
+++ domains/delivery/src/Program.cs
@@ -1,11 +1,18 @@
 using Delivery.Repository;
+using JorgeSerrano.Json;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+  .AddJsonOptions(
+      options =>
+        {
+          options.JsonSerializerOptions.PropertyNamingPolicy =
+             new JsonSnakeCaseNamingPolicy();
+        });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
