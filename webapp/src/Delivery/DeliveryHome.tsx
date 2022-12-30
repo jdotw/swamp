043724153675305f134c15300f12b76ee75a3373
James Wilson <jw@jameswilson.io>
2022-12-30 11:34:16 +1100

    Just a home, not the tribe list

--- webapp/src/Delivery/DeliveryHome.tsx
+++ webapp/src/Delivery/DeliveryHome.tsx
@@ -1,11 +1,8 @@
+import { Outlet } from "react-router-dom";
 import TribeList from "./Tribes/TribeList";
 
 function DeliveryHome() {
-  return (
-    <div>
-      <TribeList />
-    </div>
-  );
+  return <div>Delivery Home</div>;
 }
 
 export default DeliveryHome;
