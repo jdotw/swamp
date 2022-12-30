da4dadbedd0d87a2b110c3212c93cd7f3606433e
James Wilson <jw@jameswilson.io>
2022-12-30 21:38:57 +1100

    Created and ran InitialCreate migration

---
+++ domains/capability/src/Migrations/20221230103802_InitialCreate.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Capability.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chapter_role_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_chapter_role_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "practice_role_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_practice_role_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "practices",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    formeddate = table.Column<DateTimeOffset>(name: "formed_date", type: "timestamp with time zone", nullable: false),
+                    disbandeddate = table.Column<DateTimeOffset>(name: "disbanded_date", type: "timestamp with time zone", nullable: true),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_practices", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "chapters",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    formeddate = table.Column<DateTimeOffset>(name: "formed_date", type: "timestamp with time zone", nullable: false),
+                    disbandeddate = table.Column<DateTimeOffset>(name: "disbanded_date", type: "timestamp with time zone", nullable: true),
+                    practiceid = table.Column<int>(name: "practice_id", type: "integer", nullable: false),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_chapters", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_chapters_practices_practice_id",
+                        column: x => x.practiceid,
+                        principalTable: "practices",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "practice_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    individualid = table.Column<int>(name: "individual_id", type: "integer", nullable: false),
+                    practiceid = table.Column<int>(name: "practice_id", type: "integer", nullable: false),
+                    practiceroletypeid = table.Column<int>(name: "practice_role_type_id", type: "integer", nullable: false),
+                    startdate = table.Column<DateTimeOffset>(name: "start_date", type: "timestamp with time zone", nullable: false),
+                    enddate = table.Column<DateTimeOffset>(name: "end_date", type: "timestamp with time zone", nullable: true),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_practice_roles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_practice_roles_practice_role_types_practice_role_type_id",
+                        column: x => x.practiceroletypeid,
+                        principalTable: "practice_role_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_practice_roles_practices_practice_id",
+                        column: x => x.practiceid,
+                        principalTable: "practices",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "chapter_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    individualid = table.Column<int>(name: "individual_id", type: "integer", nullable: false),
+                    chapterroletypeid = table.Column<int>(name: "chapter_role_type_id", type: "integer", nullable: false),
+                    chapterid = table.Column<int>(name: "chapter_id", type: "integer", nullable: false),
+                    startdate = table.Column<DateTimeOffset>(name: "start_date", type: "timestamp with time zone", nullable: false),
+                    enddate = table.Column<DateTimeOffset>(name: "end_date", type: "timestamp with time zone", nullable: true),
+                    createddate = table.Column<DateTimeOffset>(name: "created_date", type: "timestamp with time zone", nullable: false),
+                    updateddate = table.Column<DateTimeOffset>(name: "updated_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_chapter_roles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_chapter_roles_chapter_role_types_chapter_role_type_id",
+                        column: x => x.chapterroletypeid,
+                        principalTable: "chapter_role_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_chapter_roles_chapters_chapter_id",
+                        column: x => x.chapterid,
+                        principalTable: "chapters",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chapter_roles_chapter_id",
+                table: "chapter_roles",
+                column: "chapter_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chapter_roles_chapter_role_type_id",
+                table: "chapter_roles",
+                column: "chapter_role_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chapters_practice_id",
+                table: "chapters",
+                column: "practice_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_practice_roles_practice_id",
+                table: "practice_roles",
+                column: "practice_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_practice_roles_practice_role_type_id",
+                table: "practice_roles",
+                column: "practice_role_type_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chapter_roles");
+
+            migrationBuilder.DropTable(
+                name: "practice_roles");
+
+            migrationBuilder.DropTable(
+                name: "chapter_role_types");
+
+            migrationBuilder.DropTable(
+                name: "chapters");
+
+            migrationBuilder.DropTable(
+                name: "practice_role_types");
+
+            migrationBuilder.DropTable(
+                name: "practices");
+        }
+    }
+}
