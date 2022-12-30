1ee1d0bb7583808b89e40dbd30bde63239445119
James Wilson <jw@jameswilson.io>
2022-12-30 21:26:59 +1100

    Json options, automapper and http client

--- domains/capability/src/Program.cs
+++ domains/capability/src/Program.cs
@@ -1,19 +1,31 @@
+using JorgeSerrano.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+  .AddJsonOptions(
+      options =>
+        {
+          options.JsonSerializerOptions.PropertyNamingPolicy =
+             new JsonSnakeCaseNamingPolicy();
+        });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
