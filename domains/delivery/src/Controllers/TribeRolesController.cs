24c1eeed5e27ad48e9e86f4019c45dd5419e6fb8
James Wilson <jw@jameswilson.io>
2022-12-30 14:39:19 +1100

    TribeRolesController fetches individuals details from people service

--- domains/delivery/src/Controllers/TribeRolesController.cs
+++ domains/delivery/src/Controllers/TribeRolesController.cs
@@ -1,7 +1,12 @@
+using System.Collections.Concurrent;
+using System.Text.Json;
 using AutoMapper;
 using Delivery.DTOs;
 using Delivery.Entities;
+using JorgeSerrano.Json;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 
 namespace Delivery.Controllers;
 
@@ -11,11 +16,17 @@ public class TribeRolesController : ControllerBase
 {
   private readonly ITribeRoleRepository _repository;
   private readonly IMapper _mapper;
+  private readonly ILogger<TribeRolesController> _logger;
+  private readonly IHttpClientFactory _httpClientFactory;
+  private readonly JsonSerializerOptions _jsonSerializerOptions;
 
-  public TribeRolesController(ITribeRoleRepository repository, IMapper mapper)
+  public TribeRolesController(ITribeRoleRepository repository, IMapper mapper, ILogger<TribeRolesController> logger, IHttpClientFactory httpClientFactory, IOptions<JsonOptions> jsonOptions)
   {
     _repository = repository;
     _mapper = mapper;
+    _logger = logger;
+    _httpClientFactory = httpClientFactory;
+    _jsonSerializerOptions = jsonOptions.Value.JsonSerializerOptions;
   }
 
   // GET: /tribes/1/roles
@@ -24,7 +35,21 @@ public class TribeRolesController : ControllerBase
   {
     var roles = await _repository.GetAllAsync(tribeId);
     var rolesDto = _mapper.Map<IEnumerable<TribeRoleDto>>(roles);
+    var individualIds = rolesDto.Select(r => r.IndividualId).Distinct();
+    var individualsById = await GetIndividuals(individualIds.ToList());
+    foreach (var role in rolesDto)
+    {
+      role.Individual = individualsById[role.IndividualId];
+    }
     return Ok(rolesDto);
+  }
+
+  private async Task<Dictionary<int, TribeRoleDtoIndividual>> GetIndividuals(List<int> ids)
+  {
+    var httpClient = _httpClientFactory.CreateClient();
+    var queryString = string.Join("&", ids.Select(id => $"id={id}"));
+    var individuals = await httpClient.GetFromJsonAsync<List<TribeRoleDtoIndividual>>($"http://localhost:5187/individuals?{queryString}", _jsonSerializerOptions);
+    return (individuals ?? new()).ToDictionary(i => i.Id);
   }
 
   // GET: /tribes/1/roles/5
