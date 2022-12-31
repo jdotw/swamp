597f6c28d0ccd5a9482731178bc230ccd35c2b4b
James Wilson <jw@jameswilson.io>
2023-01-01 10:19:31 +1100

    Added AddSquad and UpdateSquad DTO

--- domains/delivery/src/DTOs/SquadDTO.cs
+++ domains/delivery/src/DTOs/SquadDTO.cs
@@ -7,3 +7,16 @@ public class SquadDto
   public DateTimeOffset FormedDate { get; set; }
   public DateTimeOffset? DisbandedDate { get; set; }
 }
+
+public class AddSquadDto
+{
+  public required string Name { get; set; }
+  public DateTimeOffset FormedDate { get; set; } = DateTimeOffset.UtcNow;
+  public DateTimeOffset? DisbandedDate { get; set; }
+}
+
+public class UpdateSquadDto
+{
+  public required string Name { get; set; }
+  public DateTimeOffset? DisbandedDate { get; set; }
+}
