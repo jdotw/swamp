b0cba519860f562215255edb519a7d472c1e59b2
James Wilson <jw@jameswilson.io>
2023-01-01 10:42:36 +1100

    Add required identity mapping

--- domains/people/src/Helpers/AutoMapperProfiles.cs
+++ domains/people/src/Helpers/AutoMapperProfiles.cs
@@ -12,6 +12,7 @@ public class AutoMapperProfiles : Profile
     .ForMember(dest => dest.FirstName, opt => opt.MapFrom(c => c.Identities[0].FirstName))
     .ForMember(dest => dest.LastName, opt => opt.MapFrom(c => c.Identities[0].LastName))
     .ForMember(dest => dest.MiddleNames, opt => opt.MapFrom(c => c.Identities[0].MiddleNames));
+    CreateMap<List<Identity>, IndividualDto>();
 
     CreateMap<MutateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
       FirstName = c.FirstName,
