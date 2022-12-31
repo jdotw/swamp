3e8476e03a835282e9a25ebc65528e4946394724
James Wilson <jw@jameswilson.io>
2023-01-01 10:37:20 +1100

    Reduced number of mappings

--- domains/people/src/Helpers/AutoMapperProfiles.cs
+++ domains/people/src/Helpers/AutoMapperProfiles.cs
@@ -8,20 +8,19 @@ public class AutoMapperProfiles : Profile
 {
   public AutoMapperProfiles()
   {
-    CreateMap<MutateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
-      FirstName = c.FirstName,
-      LastName = c.LastName,
-      MiddleNames = c.MiddleNames
-    } }));
-    CreateMap<Individual, IndividualIdentifiersDto>();
-    CreateMap<List<Identity>, IndividualDto>();
     CreateMap<Individual, IndividualDto>().IncludeMembers(c => c.Identities)
     .ForMember(dest => dest.FirstName, opt => opt.MapFrom(c => c.Identities[0].FirstName))
     .ForMember(dest => dest.LastName, opt => opt.MapFrom(c => c.Identities[0].LastName))
     .ForMember(dest => dest.MiddleNames, opt => opt.MapFrom(c => c.Identities[0].MiddleNames));
 
+    CreateMap<MutateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
+      FirstName = c.FirstName,
+      LastName = c.LastName,
+      MiddleNames = c.MiddleNames
+    } }));
+
     CreateMap<LineManager, LineManagerDto>();
     CreateMap<AddLineManagerDto, LineManager>();
-    CreateMap<MutateLineManagerDto, LineManager>();
+    CreateMap<UpdateLineManagerDto, LineManager>();
   }
 }
