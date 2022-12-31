4ae95c0e682b085ce5b45c81609bf16f60525caa
James Wilson <jw@jameswilson.io>
2023-01-01 10:51:49 +1100

    Map new DTOs

--- domains/people/src/Helpers/AutoMapperProfiles.cs
+++ domains/people/src/Helpers/AutoMapperProfiles.cs
@@ -14,14 +14,19 @@ public class AutoMapperProfiles : Profile
     .ForMember(dest => dest.MiddleNames, opt => opt.MapFrom(c => c.Identities[0].MiddleNames));
     CreateMap<List<Identity>, IndividualDto>();
 
-    CreateMap<MutateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
+    CreateMap<CreateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
+      FirstName = c.FirstName,
+      LastName = c.LastName,
+      MiddleNames = c.MiddleNames
+    } }));
+    CreateMap<UpdateIndividualDto, Individual>().ForMember(dest => dest.Identities, opt => opt.MapFrom(c => new List<Identity> { new Identity {
       FirstName = c.FirstName,
       LastName = c.LastName,
       MiddleNames = c.MiddleNames
     } }));
 
     CreateMap<LineManager, LineManagerDto>();
-    CreateMap<AddLineManagerDto, LineManager>();
+    CreateMap<CreateLineManagerDto, LineManager>();
     CreateMap<UpdateLineManagerDto, LineManager>();
   }
 }
