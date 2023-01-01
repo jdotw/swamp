56d7a7fd25478dcfa4d317261def06864b4cde0e
James Wilson <jw@jameswilson.io>
2023-01-01 11:20:34 +1100

    First integration test

--- domains/delivery/integration_tests/UnitTest1.cs
+++ domains/delivery/integration_tests/UnitTest1.cs
@@ -1,10 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace integration_tests;
 
-public class UnitTest1
+public class BasicTests
+    : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
-    {
+  private readonly WebApplicationFactory<Program> _factory;
 
-    }
+  public BasicTests(WebApplicationFactory<Program> factory)
+  {
+    _factory = factory;
+  }
+
+  [Theory]
+  [InlineData("/tribes")]
+  public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+  {
+    // Arrange
+    var client = _factory.CreateClient();
+
+    // Act
+    var response = await client.GetAsync(url);
+
+    // Assert
+    response.EnsureSuccessStatusCode(); // Status Code 200-299
+    Assert.Equal("application/json; charset=utf-8",
+        response.Content.Headers.ContentType.ToString());
+  }
 }
