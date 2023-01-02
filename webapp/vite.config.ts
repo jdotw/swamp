776312b5ae952354851740441e569879cddfc363
James Wilson <jw@jameswilson.io>
2023-01-02 11:47:50 +1100

    Added vitest

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -23,5 +25,13 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api\/delivery/, ""),
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
   },
 });
