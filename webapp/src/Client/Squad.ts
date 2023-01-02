eae4845dd5e23805eec4a7f5b656d0701926f61b
James Wilson <jw@jameswilson.io>
2023-01-02 14:37:06 +1100

    Refactored useSquad to match useChapter

--- webapp/src/Client/Squad.ts
+++ webapp/src/Client/Squad.ts
@@ -8,48 +8,92 @@ export type Squad = {
   lead_full_name: string;
 };
 
+export type SquadRole = {
+  id: string;
+  individual: Individual;
+  individual_id: string;
+  squad_id: string;
+  squad_role_type_id: string;
+  // squad_role_type: SquadRoleType;
+};
+
+export type NewSquadRole = {
+  tribe_id: string;
+  squad_id: string;
+  individual_id: string;
+  squad_role_type_id: string;
+};
+
 export interface UseSquadProps {
   tribeId?: string;
   id?: string;
 }
 
 export function useSquad({ tribeId, id }: UseSquadProps) {
+  const [squad, setSquad] = useState<Squad>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(undefined);
-  const [members, setMembers] = useState<Individual[]>([]);
-  // const [addError, setAddError] = useState(undefined);
-  // const [adding, setAdding] = useState(false);
 
-  const { getAll: getMembers } = useCRUD<Individual, undefined>({
-    path: `/api/delivery/tribes/${tribeId}/squads/${id}/members`,
+  const [roles, setRoles] = useState<SquadRole[]>([]);
+  const [loadingRoles, setLoadingRoles] = useState(false);
+  const [loadingRolesError, setLoadingRolesError] = useState(undefined);
+
+  const { getAll: getAllRoleItems, createItem: createRoleItem } = useCRUD<
+    SquadRole,
+    NewSquadRole
+  >({
+    path: `/api/delivery/tribes/${tribeId}/squads/${id}/roles`,
   });
 
-  // const addSquad = async (roleType: NewSquad) => {
-  //   setAdding(true);
-  //   try {
-  //     const newRoleType = await createItem(roleType);
-  //     setMembers([...members, newRoleType]);
-  //   } catch (error: any) {
-  //     setAddError(error);
-  //   } finally {
-  //     setAdding(false);
-  //   }
-  // };
+  const { retrieveItem } = useCRUD<Squad, undefined>({
+    path: `/api/delivery/tribes/${tribeId}/squads`,
+  });
+
+  const load = async () => {
+    try {
+      const squad = await retrieveItem(id!);
+      setSquad(squad);
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadRoles = async () => {
+    setLoadingRoles(true);
+    try {
+      const roles = await getAllRoleItems();
+      setRoles(roles);
+      setLoadingRolesError(undefined);
+    } catch (error: any) {
+      setLoadingRolesError(error);
+    } finally {
+      setLoadingRoles(false);
+    }
+  };
+
+  const addRole = async (newRole: NewSquadRole) => {
+    const role = await createRoleItem(newRole);
+    setRoles([...roles, role]);
+  };
 
   useEffect(() => {
     if (tribeId && id) {
       (async () => {
-        try {
-          const members = await getMembers();
-          setMembers(members);
-        } catch (error: any) {
-          setError(error);
-        } finally {
-          setLoading(false);
-        }
+        await load();
+        await loadRoles();
       })();
     }
   }, [tribeId, id]);
 
-  return { loading, members, error };
+  return {
+    squad,
+    loading,
+    error,
+    roles,
+    loadingRoles,
+    loadingRolesError,
+    addRole,
+  };
 }
