895c49061a2242b1869e71112b59a0c161ecb4e7
James Wilson <jw@jameswilson.io>
2023-01-04 12:49:29 +1100

    Added CRUD integrations for SquadRoles

--- domains/delivery/integration_tests/SquadRoles.cs
+++ domains/delivery/integration_tests/SquadRoles.cs
@@ -19,9 +19,14 @@ public class SquadRoleTests
     Console.WriteLine("At Constructor _seedData.ExistingSquadRoleTypeId: " + _seedData.ExistingSquadRoleTypeId);
   }
 
+  private string _basePath(int? tribeId = null, int? squadId = null)
+  {
+    return $"/tribes/{tribeId ?? _seedData.ExistingTribeId}/squads/{squadId ?? _seedData.ExistingSquadId}/roles";
+  }
+
   private async Task<SquadRoleDto?> CreateSquadRole(int tribeId, int individualId, int squadRoleTypeId, int squadId)
   {
-    var role = await _client.PostAsJsonAsync($"/tribes/{tribeId}/squads/{squadId}/roles",
+    var role = await _client.PostAsJsonAsync(_basePath(),
       new AddSquadRoleDto { IndividualId = individualId, SquadRoleTypeId = squadRoleTypeId }, _options)
        .ContinueWith<SquadRoleDto?>(t => t.Result.Content.ReadFromJsonAsync<SquadRoleDto>(_options).Result);
     return role;
@@ -48,75 +53,84 @@ public class SquadRoleTests
     Assert.Equal(role.Squad.Id, squadId);
   }
 
-  //   [Fact]
-  //   public async Task TestGetAllSquads()
-  //   {
-  //     // Arrange
-  //     var name = "Seen In Get All";
-  //     var squad = await CreateSquad(name);
+  [Fact]
+  public async Task TestGetAllSquadRoles()
+  {
+    // Arrange
+    var individualId = 1;
+    var expectedSquad = await CreateSquadRole(_seedData.ExistingTribeId,
+                                              individualId,
+                                              _seedData.ExistingSquadRoleTypeId,
+                                              _seedData.ExistingSquadId);
 
-  //     // Act
-  //     var squads = await _client.GetFromJsonAsync<List<Squad>>($"{_path}", _options);
+    // Act
+    Console.WriteLine("BASE: " + _basePath());
+    var squads = await _client.GetFromJsonAsync<List<SquadRoleDto>>(_basePath(), _options);
 
-  //     // Assert
-  //     Assert.NotNull(squads);
-  //     Assert.Contains(squads, t => t.Name == name);
-  //   }
+    // Assert
+    Assert.NotNull(squads);
+    Assert.Contains(squads, t => t.Id == expectedSquad!.Id);
+  }
 
-  //   [Fact]
-  //   public async Task TestGetSquad()
-  //   {
-  //     // Arrange
-  //     var name = "Single Get Squad";
-  //     var createdSquad = await CreateSquad(name);
-  //     Assert.NotNull(createdSquad);
+  [Fact]
+  public async Task TestGetSquadRole()
+  {
+    // Arrange
+    var individualId = 1;
+    var expectedSquad = await CreateSquadRole(_seedData.ExistingTribeId,
+                                              individualId,
+                                              _seedData.ExistingSquadRoleTypeId,
+                                              _seedData.ExistingSquadId);
 
-  //     // Act
-  //     var squad = await _client.GetFromJsonAsync<SquadDto>($"{_path}/{createdSquad.Id}", _options);
+    Assert.NotNull(expectedSquad);
 
-  //     // Assert
-  //     Assert.NotNull(squad);
-  //     squad.Name.Should().Be(name);
-  //   }
+    // Act
+    var role = await _client.GetFromJsonAsync<SquadRoleDto>($"{_basePath()}/{expectedSquad.Id}", _options);
 
-  //   [Fact]
-  //   public async Task TestUpdateSquad()
-  //   {
-  //     // Arrange
-  //     var existingSquadId = 1;  // Seeded for update
-  //     var path = $"{_path}/{existingSquadId}";
+    // Assert
+    Assert.NotNull(role);
+    role.Id.Should().Be(expectedSquad.Id);
+    role.IndividualId.Should().Be(expectedSquad.IndividualId);
+    role.SquadRoleType.Id.Should().Be(expectedSquad.SquadRoleType.Id);
+  }
 
-  //     // Act
-  //     var updateDto = new UpdateSquadDto
-  //     {
-  //       Name = "Updated Squad",
-  //       DisbandedDate = DateTime.UtcNow
-  //     };
-  //     var response = await _client.PutAsJsonAsync(path, updateDto, _options);
-  //     var updatedSquad = await _client.GetFromJsonAsync<Squad>(path, _options);
+  [Fact]
+  public async Task TestUpdateSquadRole()
+  {
+    // Arrange
+    var squadId = _seedData.SquadRoleToUpdateId;
+    var path = $"{_basePath()}/{squadId}";
 
-  //     // Assert
-  //     Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-  //     Assert.NotNull(updatedSquad);
-  //     Assert.Equal(updateDto.Name, updatedSquad.Name);
-  //     Assert.Equal(updateDto.DisbandedDate, updatedSquad.DisbandedDate);
-  //   }
+    // Act
+    var updateDto = new UpdateSquadRoleDto
+    {
+      EndDate = DateTime.UtcNow
+    };
+    var response = await _client.PutAsJsonAsync(path, updateDto, _options);
+    var updatedSquad = await _client.GetFromJsonAsync<SquadRoleDto>(path, _options);
 
-  //   [Fact]
-  //   public async Task TestDeleteSquad()
-  //   {
-  //     // Arrange
-  //     var existingSquadId = 2;  // Seeded for delete
-  //     var path = $"{_path}/{existingSquadId}";
+    // Assert
+    Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    Assert.NotNull(updatedSquad);
+    Assert.Equal(updateDto.EndDate, updatedSquad.EndDate);
+  }
 
-  //     // Act
-  //     var response = await _client.DeleteAsync(path);
+  [Fact]
+  public async Task TestDeleteSquad()
+  {
+    // Arrange
+    var squadId = _seedData.SquadRoleToDeleteId;
+    var path = $"{_basePath()}/{squadId}";
 
-  //     // Assert
-  //     Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-  //     // var ex = await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(() => _client.GetFromJsonAsync<Squad>(_path, _options));
-  //     // Assert.Contains("404 (Not Found)", ex.Message);
-  //   }
+    // Act
+    var response = await _client.DeleteAsync(path);
+
+    // Assert
+    Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    var ex = await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(
+      () => _client.GetFromJsonAsync<Squad>(path, _options));
+    Assert.Contains("404 (Not Found)", ex.Message);
+  }
 }
 
 public class SquadRolesSeedDataClass : ISeedDataClass
@@ -124,6 +138,8 @@ public class SquadRolesSeedDataClass : ISeedDataClass
   public int ExistingTribeId;
   public int ExistingSquadId;
   public int ExistingSquadRoleTypeId;
+  public int SquadRoleToUpdateId;
+  public int SquadRoleToDeleteId;
 
   public void InitializeDbForTests(DeliveryDbContext db)
   {
@@ -164,8 +180,22 @@ public class SquadRolesSeedDataClass : ISeedDataClass
     db.SaveChanges(true);
     ExistingSquadRoleTypeId = squadRoleTypeResult.Entity.Id;
 
-    Console.WriteLine("SquadRolesSeedDataClass.ExistingSquadRoleTypeId: " + ExistingSquadRoleTypeId);
+    var squadRoleToUpdateResult = db.SquadRoles.Add(new SquadRole
+    {
+      IndividualId = 33,
+      SquadId = ExistingSquadId,
+      SquadRoleTypeId = ExistingSquadRoleTypeId
+    });
+    db.SaveChanges(true);
+    SquadRoleToUpdateId = squadRoleToUpdateResult.Entity.Id;
 
-    db.SquadRoleTypes.ToList().ForEach(t => Console.WriteLine(t.Id));
+    var squadRoleToDeleteResult = db.SquadRoles.Add(new SquadRole
+    {
+      IndividualId = 44,
+      SquadId = ExistingSquadId,
+      SquadRoleTypeId = ExistingSquadRoleTypeId
+    });
+    db.SaveChanges(true);
+    SquadRoleToDeleteId = squadRoleToDeleteResult.Entity.Id;
   }
 }
