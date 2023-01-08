ce72db0cdfe924a3dd5b2f6ae3d24fe7972e3278
James Wilson <jw@jameswilson.io>
2023-01-08 21:17:20 +1100

    Refactored ChapterRoleType to use useCRUD and added tests

---
+++ webapp/src/Client/ChapterRoleType.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useCRUD, UseCRUDOptionalProps } from "../CRUD/CRUD";
+
+export type ChapterRoleType = {
+  id: string;
+  name: string;
+};
+
+export type MutateChapterRoleType = {
+  name: string;
+};
+
+export interface UseChapterRoleTypeProps extends UseCRUDOptionalProps {}
+
+export function useChapterRoleType(props: UseChapterRoleTypeProps) {
+  const {
+    items,
+    loading,
+    error,
+    reload,
+    createItem,
+    retrieveItem,
+    updateItem,
+    deleteItem,
+  } = useCRUD<ChapterRoleType, MutateChapterRoleType>({
+    ...props,
+    path: "/api/capability/chapterroletypes",
+  });
+
+  return {
+    items,
+    loading,
+    error,
+    reload,
+    createItem,
+    retrieveItem,
+    updateItem,
+    deleteItem,
+  };
+}
