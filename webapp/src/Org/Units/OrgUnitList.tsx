74dbc5968f6a0d6aa832a20940c3616a05b7de90
James Wilson <jw@jameswilson.io>
2023-01-09 22:37:42 +1100

    Progressed OrgUnitList and tests

--- webapp/src/Org/Units/OrgUnitList.tsx
+++ webapp/src/Org/Units/OrgUnitList.tsx
@@ -1,22 +1,50 @@
-import { ScrollArea, Table } from "@mantine/core";
-import React from "react";
+import { Button, ScrollArea, Table } from "@mantine/core";
+import { useState } from "react";
+import { useOrg } from "../../Client/Org";
+import Loading from "../../Loading/Loading";
+import MutateOrgUnitModal from "./MutateOrgUnitModal";
+import { MutateOrgUnitModalMode } from "./MutateOrgUnitModal";
 
 function OrgUnitList() {
+  const { loading, items } = useOrg();
+  const [mutateOrgUnitModalOpen, setMutateOrgUnitModalOpen] = useState(false);
+  const [mutateOrgUnitModalMode, setMutateOrgUnitModalMode] = useState(
+    MutateOrgUnitModalMode.Create
+  );
+  const orgRows = items.map((org) => (
+    <tr key={org.id}>
+      <td>{org.name}</td>
+    </tr>
+  ));
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <div>
-      <title>Organisation Units</title>
-      <ScrollArea>
-        <Table verticalSpacing="xs">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Parent</th>
-            </tr>
-          </thead>
-          {/* <tbody>{rows}</tbody> */}
-        </Table>
-      </ScrollArea>
-    </div>
+    <>
+      <div>
+        <title>Organisation Units</title>
+        <ScrollArea>
+          <Table verticalSpacing="xs" data-testid={"org-units-table"}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Parent</th>
+              </tr>
+            </thead>
+            <tbody>{orgRows}</tbody>
+          </Table>
+        </ScrollArea>
+        <Button
+          data-testid={"add-org-unit-button"}
+          onClick={() => setMutateOrgUnitModalOpen(true)}
+        >
+          Add Org Unit
+        </Button>
+      </div>
+      <MutateOrgUnitModal opened={mutateOrgUnitModalOpen} />
+    </>
   );
 }
 
