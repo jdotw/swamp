de760f573fa31233998a933aac4b856307852f83
James Wilson <jw@jameswilson.io>
2023-01-10 08:07:17 +1100

    Added cancel and submit functions

--- webapp/src/Org/Units/OrgUnitList.tsx
+++ webapp/src/Org/Units/OrgUnitList.tsx
@@ -1,16 +1,17 @@
 import { Button, ScrollArea, Table } from "@mantine/core";
 import { useState } from "react";
-import { useOrg } from "../../Client/Org";
+import { MutateOrg, Org, useOrg } from "../../Client/Org";
 import Loading from "../../Loading/Loading";
 import MutateOrgUnitModal from "./MutateOrgUnitModal";
 import { MutateOrgUnitModalMode } from "./MutateOrgUnitModal";
 
 function OrgUnitList() {
-  const { loading, items } = useOrg();
+  const { loading, items, createItem, updateItem } = useOrg();
   const [mutateOrgUnitModalOpen, setMutateOrgUnitModalOpen] = useState(false);
   const [mutateOrgUnitModalMode, setMutateOrgUnitModalMode] = useState(
     MutateOrgUnitModalMode.Create
   );
+  const [orgBeingEdited, setOrgBeingEdited] = useState<Org>();
   const orgRows = items.map((org) => (
     <tr key={org.id}>
       <td>{org.name}</td>
@@ -20,6 +21,19 @@ function OrgUnitList() {
   if (loading) {
     return <Loading />;
   }
+
+  const onMutateOrgSubmit = async (mutatedOrg: MutateOrg) => {
+    if (mutateOrgUnitModalMode === MutateOrgUnitModalMode.Create) {
+      console.log("CREATE: ", mutatedOrg);
+      createItem(mutatedOrg);
+    } else if (mutateOrgUnitModalMode === MutateOrgUnitModalMode.Edit) {
+      console.log("EDIT: ", mutatedOrg);
+      if (orgBeingEdited) {
+        updateItem(orgBeingEdited.id, mutatedOrg);
+      }
+    }
+    setMutateOrgUnitModalOpen(false);
+  };
 
   return (
     <>
@@ -43,7 +57,12 @@ function OrgUnitList() {
           Add Org Unit
         </Button>
       </div>
-      <MutateOrgUnitModal opened={mutateOrgUnitModalOpen} />
+      <MutateOrgUnitModal
+        opened={mutateOrgUnitModalOpen}
+        mode={mutateOrgUnitModalMode}
+        onClose={() => setMutateOrgUnitModalOpen(false)}
+        onSubmit={onMutateOrgSubmit}
+      />
     </>
   );
 }
