6dd047f65fd7b9808477b93eead0776f8a8d3e3d
James Wilson <jw@jameswilson.io>
2023-01-10 08:06:34 +1100

    Updated Org test to use useCRUD tests

--- webapp/src/Client/Org.test.ts
+++ webapp/src/Client/Org.test.ts
@@ -3,6 +3,7 @@ import { act } from "react-dom/test-utils";
 import { Mock, vi } from "vitest";
 import { useCRUD } from "../CRUD/CRUD";
 import { MutateOrg, Org, useOrg, UseOrgProps } from "./Org";
+import { expectHookAdoptsUseCRUDCorrectly } from "./UseCRUDTestHelpers";
 
 interface RenderUseOrgHookProps extends UseOrgProps {}
 
@@ -17,121 +18,19 @@ vi.mock("../CRUD/CRUD", () => {
   };
 });
 
-const mockReload = vi.fn();
-const mockCreateItem = vi.fn();
-const mockRetrieveItem = vi.fn();
-const mockUpdateItem = vi.fn();
-const mockDeleteItem = vi.fn();
-(useCRUD as Mock<any[], any>).mockReturnValue({
-  reload: mockReload,
-  retrieveItem: mockRetrieveItem,
-  createItem: mockCreateItem,
-  updateItem: mockUpdateItem,
-  deleteItem: mockDeleteItem,
-});
-
-const path = "/api/people/orgs";
-
-describe("useOrg hook", () => {
-  it("should call useCRUD with expected parameters", async () => {
-    await renderUseOrgHook({ loadOnMount: true });
-    expect(useCRUD).toBeCalledTimes(1);
-    expect(useCRUD).toBeCalledWith({
-      path,
-      loadOnMount: true,
-    });
-  });
-  describe("when loadOnMount=false", () => {
-    it("should call useCRUD with loadOnMount=false", async () => {
-      await renderUseOrgHook({ loadOnMount: false });
-      expect(useCRUD).toBeCalledTimes(1);
-      expect(useCRUD).toBeCalledWith({
-        path,
-        loadOnMount: false,
-      });
-    });
-  });
-
-  it("should return expected parameters", async () => {
-    const hook = await renderUseOrgHook();
-    expect(hook.result.current).toHaveProperty("reload");
-
-    expect(hook.result.current).toHaveProperty("items");
-    expect(hook.result.current).toHaveProperty("loading");
-    expect(hook.result.current).toHaveProperty("error");
-
-    expect(hook.result.current).toHaveProperty("createItem");
-    expect(hook.result.current).toHaveProperty("updateItem");
-    expect(hook.result.current).toHaveProperty("deleteItem");
-  });
-  describe("reload function", () => {
-    it("should call useCRUD's reload function", async () => {
-      const hook = await renderUseOrgHook();
-      act(() => {
-        hook.result.current.reload();
-      });
-      expect(mockReload).toBeCalledTimes(1);
-    });
-  });
-  describe("createItem function", () => {
-    it("should call useCRUD's createItem function", async () => {
-      const newItem = {
-        name: "New Org",
-        parent_id: "123",
-      } as MutateOrg;
-      const hook = await renderUseOrgHook();
-      act(() => {
-        hook.result.current.createItem(newItem);
-      });
-      expect(mockCreateItem).toBeCalledWith(newItem);
-    });
-  });
-  describe("retrieveItem function", () => {
-    it("should call useCRUD's retrieveItem function", async () => {
-      const itemId = Math.floor(Math.random() * 1000).toString();
-      const expectedItem = {
-        id: itemId,
-        name: "New Org",
-      };
-      const hook = await renderUseOrgHook();
-      mockRetrieveItem.mockResolvedValueOnce(expectedItem);
-      await act(async () => {
-        const result = await hook.result.current.retrieveItem(itemId);
-        expect(result).toBe(expectedItem);
-      });
-      expect(mockRetrieveItem).toBeCalledWith(itemId);
-    });
-  });
-  describe("updateItem function", () => {
-    it("should call useCRUD's updateItem function", async () => {
-      const itemId = Math.floor(Math.random() * 1000).toString();
-      const itemMutation = {
-        name: "Updated Org Name",
-      };
-      const hook = await renderUseOrgHook();
-      const expectedResult = true;
-      mockUpdateItem.mockResolvedValueOnce(expectedResult);
-      await act(async () => {
-        const result = await hook.result.current.updateItem(
-          itemId,
-          itemMutation
-        );
-        expect(result).toBe(expectedResult);
-      });
-      expect(mockUpdateItem).toBeCalledWith(itemId, itemMutation);
-    });
-  });
-  describe("deleteItem function", () => {
-    it("should call useCRUD's deleteItem function", async () => {
-      const itemId = Math.floor(Math.random() * 1000).toString();
-      const hook = await renderUseOrgHook();
-      const expectedResult = true;
-      mockDeleteItem.mockResolvedValueOnce(expectedResult);
-      await act(async () => {
-        const result = await hook.result.current.deleteItem(itemId);
-        expect(result).toBe(expectedResult);
-      });
-      expect(mockDeleteItem).toBeCalledWith(itemId);
+describe("useOrg Hook", () => {
+  it("should use useCRUD correctly", () => {
+    expectHookAdoptsUseCRUDCorrectly({
+      hookFactory: () => useOrg(),
+      itemFactory: () =>
+        ({
+          id: Math.floor(Math.random() * 1000).toString(),
+          name: "Test Org",
+        } as Org),
+      mutateItemFactory: () =>
+        ({
+          name: "Mutated Test Org",
+        } as Org),
     });
   });
 });
