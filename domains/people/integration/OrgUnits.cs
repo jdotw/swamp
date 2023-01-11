6b7f7a985e700de0446f5e4e66799f0d5e2d34ae
James Wilson <jw@jameswilson.io>
2023-01-12 07:57:08 +1100

    - simplified seed data (only need one org unit present) - seed is performed per-test - enabled all tests - ensure delete matches on ID, not name

--- domains/people/integration/OrgUnits.cs
+++ domains/people/integration/OrgUnits.cs
@@ -19,78 +19,78 @@ public class OrgUnitTests
   {
   }
 
-  // [Fact]
-  // public async Task TestCreateOrgUnit()
-  // {
-  //   // Arrange
-  //   var testStart = DateTime.UtcNow;
-  //   var name = "Test OrgUnit Creation";
+  [Fact]
+  public async Task TestCreateOrgUnit()
+  {
+    // Arrange
+    var testStart = DateTime.UtcNow;
+    var name = "Test OrgUnit Creation";
 
-  //   // Act
-  //   var orgUnit = await _client.PostAsJsonAsync(_path, new MutateOrgUnitDto { Name = name }, _options)
-  //     .ContinueWith<OrgUnitDto?>(t => t.Result.Content.ReadFromJsonAsync<OrgUnitDto>(_options).Result);
+    // Act
+    var orgUnit = await _client.PostAsJsonAsync(_path, new MutateOrgUnitDto { Name = name }, _options)
+      .ContinueWith<OrgUnitDto?>(t => t.Result.Content.ReadFromJsonAsync<OrgUnitDto>(_options).Result);
 
-  //   // Assert
-  //   Assert.NotNull(orgUnit);
-  //   Assert.Equal(name, orgUnit.Name);
-  //   orgUnit.FormedDate.Should().BeAfter(testStart);
-  //   Assert.Null(orgUnit.DisbandedDate);
-  // }
+    // Assert
+    Assert.NotNull(orgUnit);
+    Assert.Equal(name, orgUnit.Name);
+    orgUnit.FormedDate.Should().BeOnOrAfter(testStart);
+    Assert.Null(orgUnit.DisbandedDate);
+  }
 
-  // [Fact]
-  // public async Task TestGetAllOrgUnits()
-  // {
-  //   // Arrange
-  //   // (Note: existing records added by DbSeedClass)
+  [Fact]
+  public async Task TestGetAllOrgUnits()
+  {
+    // Arrange
+    // (Note: existing records added by DbSeedClass)
 
-  //   // Act
-  //   var orgUnits = await _client.GetFromJsonAsync<List<OrgUnit>>($"{_path}", _options);
+    // Act
+    var orgUnits = await _client.GetFromJsonAsync<List<OrgUnit>>($"{_path}", _options);
 
-  //   // Assert
-  //   Assert.NotNull(orgUnits);
-  //   Assert.Contains(orgUnits, t => t.Name == _seedData.ExistingOrgUnit.Name);
-  // }
+    // Assert
+    Assert.NotNull(orgUnits);
+    Assert.Contains(orgUnits, t => t.Name == _seedData.OrgUnit.Name);
+  }
 
-  // [Fact]
-  // public async Task TestGetOrgUnit()
-  // {
-  //   // Arrange
+  [Fact]
+  public async Task TestGetOrgUnit()
+  {
+    // Arrange
 
-  //   // Act
-  //   var orgUnit = await _client.GetFromJsonAsync<OrgUnitDto>($"{_path}/{_seedData.ExistingOrgUnit.Id}", _options);
+    // Act
+    var orgUnit = await _client.GetFromJsonAsync<OrgUnitDto>($"{_path}/{_seedData.OrgUnit.Id}", _options);
 
-  //   // Assert
-  //   Assert.NotNull(orgUnit);
-  //   Assert.Equal(orgUnit.Name, _seedData.ExistingOrgUnit.Name);
-  // }
+    // Assert
+    Assert.NotNull(orgUnit);
+    Assert.Equal(orgUnit.Name, _seedData.OrgUnit.Name);
+  }
 
-  // [Fact]
-  // public async Task TestUpdateOrgUnit()
-  // {
-  //   // Arrange
-  //   var orgUnit = _seedData.OrgUnitToUpdate;
+  [Fact]
+  public async Task TestUpdateOrgUnit()
+  {
+    // Arrange
+    var orgUnit = _seedData.OrgUnit;
 
-  //   // Act
-  //   var updateDto = new MutateOrgUnitDto
-  //   {
-  //     Name = "Updated OrgUnit",
-  //     DisbandedDate = DateTime.UtcNow
-  //   };
-  //   var response = await _client.PutAsJsonAsync($"{_path}/{orgUnit.Id}", updateDto, _options);
-  //   var updatedOrgUnit = await _client.GetFromJsonAsync<OrgUnit>($"{_path}/{orgUnit.Id}", _options);
+    // Act
+    var updateDto = new MutateOrgUnitDto
+    {
+      Name = "Updated OrgUnit",
+      DisbandedDate = DateTime.UtcNow
+    };
+    var response = await _client.PutAsJsonAsync($"{_path}/{orgUnit.Id}", updateDto, _options);
+    var updatedOrgUnit = await _client.GetFromJsonAsync<OrgUnit>($"{_path}/{orgUnit.Id}", _options);
 
-  //   // Assert
-  //   Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-  //   Assert.NotNull(updatedOrgUnit);
-  //   Assert.Equal(updateDto.Name, updatedOrgUnit.Name);
-  //   Assert.Equal(updateDto.DisbandedDate, updatedOrgUnit.DisbandedDate);
-  // }
+    // Assert
+    Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    Assert.NotNull(updatedOrgUnit);
+    Assert.Equal(updateDto.Name, updatedOrgUnit.Name);
+    Assert.Equal(updateDto.DisbandedDate, updatedOrgUnit.DisbandedDate);
+  }
 
   [Fact]
   public async Task TestDeleteOrgUnit()
   {
     // Arrange
-    var orgUnit = _seedData.OrgUnitToDelete;
+    var orgUnit = _seedData.OrgUnit;
 
     // Act
     var response = await _client.DeleteAsync($"{_path}/{orgUnit.Id}");
@@ -101,42 +101,22 @@ public class OrgUnitTests
     var ex = await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(() => _client.GetFromJsonAsync<OrgUnit>($"{_path}/{orgUnit.Id}", _options));
     Assert.Contains("404 (Not Found)", ex.Message);
     Assert.NotNull(orgUnits);
-    Assert.DoesNotContain(orgUnits, t => t.Name == orgUnit.Name);
+    Assert.DoesNotContain(orgUnits, t => t.Id == orgUnit.Id);
   }
 }
 
 public class OrgUnitsSeedDataClass : ISeedDataClass<PeopleDbContext>
 {
-  public OrgUnit ExistingOrgUnit = null!;
-  public OrgUnit OrgUnitToUpdate = null!;
-  public OrgUnit OrgUnitToDelete = null!;
+  public OrgUnit OrgUnit = null!;
 
   public void InitializeDbForTests(PeopleDbContext db)
   {
     // Performs DB initialization before the 
     // start of all tests in the OrgUnitTests class.
     // The DB is not re-initialized between tests.
-    // Performs DB initialization before the 
-    // start of all tests in the SquadTests class.
-    // The DB is not re-initialized between tests.
-
-    ExistingOrgUnit = db.OrgUnits.Add(new OrgUnit
+    OrgUnit = db.OrgUnits.Add(new OrgUnit
     {
-      Name = "Existing OrgUnit",
-      FormedDate = DateTime.UtcNow,
-      DisbandedDate = null
-    }).Entity;
-
-    OrgUnitToUpdate = db.OrgUnits.Add(new OrgUnit
-    {
-      Name = "Existing OrgUnit To Update",
-      FormedDate = DateTime.UtcNow,
-      DisbandedDate = null
-    }).Entity;
-
-    OrgUnitToDelete = db.OrgUnits.Add(new OrgUnit
-    {
-      Name = "Org Unit To Delete",
+      Name = "Seed OrgUnit",
       FormedDate = DateTime.UtcNow,
       DisbandedDate = null
     }).Entity;
