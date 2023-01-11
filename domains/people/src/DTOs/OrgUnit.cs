5d32b23b7ac024b29747d6606ec00d1d6bef69ab
James Wilson <jw@jameswilson.io>
2023-01-11 22:18:22 +1100

    Added formed and disbanded dates to OrgUnit and DTOs

--- domains/people/src/DTOs/OrgUnit.cs
+++ domains/people/src/DTOs/OrgUnit.cs
@@ -4,9 +4,13 @@ public class OrgUnitDto
 {
   public int Id { get; set; }
   public string Name { get; set; } = string.Empty;
+  public DateTimeOffset FormedDate { get; set; }
+  public DateTimeOffset? DisbandedDate { get; set; }
 }
 
 public class MutateOrgUnitDto
 {
   public required string Name { get; set; } = string.Empty;
+  public DateTimeOffset FormedDate { get; set; } = DateTimeOffset.UtcNow;
+  public DateTimeOffset? DisbandedDate { get; set; }
 }
--- domains/people/src/Entities/OrgUnit.cs
+++ domains/people/src/Entities/OrgUnit.cs
@@ -5,6 +5,8 @@ namespace People.Entities;
 public class OrgUnit : EntityBase
 {
   public string? Name { get; set; }
+  public DateTimeOffset FormedDate { get; set; }
+  public DateTimeOffset? DisbandedDate { get; set; }
 
   public OrgUnit()
   {
