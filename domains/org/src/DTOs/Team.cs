da509ebf3ff4fa7f1712e7b2106f6a3a1605f649
James Wilson <jw@jameswilson.io>
2023-01-17 09:03:20 +1100

    Remove purpose

--- domains/org/src/DTOs/Team.cs
+++ domains/org/src/DTOs/Team.cs
@@ -4,15 +4,12 @@ namespace Org.DTOs;
 
 public class TeamDto : UnitDto
 {
-  public string Purpose { get; set; } = null!;
 }
 
 public class CreateTeamDto : CreateUnitDto
 {
-  public string Purpose { get; set; } = null!;
 }
 
 public class UpdateTeamDto : UpdateUnitDto
 {
-  public string Purpose { get; set; } = null!;
 }
