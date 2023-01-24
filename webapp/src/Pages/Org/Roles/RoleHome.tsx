f39f8909f0c6be85f92feacd81c4e438f39cccc5
James Wilson <jw@jameswilson.io>
2023-01-24 21:34:39 +1100

    Roles

--- webapp/src/Pages/Org/Roles/RoleHome.tsx
+++ webapp/src/Pages/Org/Roles/RoleHome.tsx
@@ -1,7 +1,113 @@
+import {
+  Checkbox,
+  createStyles,
+  Flex,
+  Select,
+  Table,
+  Title,
+} from "@mantine/core";
 import React from "react";
 
+const useStyles = createStyles((theme) => ({
+  checkboxTitleBox: {
+    display: "flex",
+    flexDirection: "row",
+    flex: 1,
+    paddingTop: 10,
+    borderBottom: "1px solid #00000040",
+  },
+  checkboxInTitle: {
+    marginRight: 10,
+    display: "flex",
+    alignItems: "center",
+  },
+  titleWithCheckbox: {},
+}));
+
+const allowedFunctions = () => (
+  <Select
+    key="role_type"
+    placeholder="select duty type"
+    defaultValue={"1"}
+    data={[
+      { value: "1", label: "None" },
+      { value: "2", label: "Member" },
+      { value: "3", label: "Manager" },
+    ]}
+  />
+);
+
 function RoleHome() {
-  return <div>RoleHome</div>;
+  const { classes, theme } = useStyles();
+
+  return (
+    <div>
+      <Title order={3}>Role Type: Software Engineer</Title>
+      <hr />
+
+      {/* <Title order={4}>Default Functions</Title>
+      <Table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Member</td>
+            <td>Squad</td>
+          </tr>
+          <tr>
+            <td>Member</td>
+            <td>Chapter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <hr />
+      <Title order={4}>Levels</Title>
+      <div className={classes.checkboxTitleBox}>
+        <Checkbox className={classes.checkboxInTitle} />
+        <Title className={classes.titleWithCheckbox} order={4}>
+          Senior
+        </Title>
+      </div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Manager</td>
+            <td>Chapter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Title order={4}>
+        <div className={classes.checkboxTitleBox}>
+          <Checkbox className={classes.checkboxInTitle} />
+          Principal
+        </div>
+      </Title>
+      <Table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Manager</td>
+            <td>Squad</td>
+          </tr>
+        </tbody>
+      </Table> */}
+    </div>
+  );
 }
 
 export default RoleHome;
--- webapp/src/Pages/Org/Roles/RolesList.tsx
+++ webapp/src/Pages/Org/Roles/RolesList.tsx
@@ -1,7 +1,82 @@
+import { ScrollArea, Table, Title } from "@mantine/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function RolesList() {
-  return <div>RolesList</div>;
+  return (
+    <div>
+      <Title order={3}>Role Types</Title>
+      <ScrollArea>
+        <Table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Vacant</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Link to={"3"}>Software Engineer</Link>
+              </td>
+              <td>3</td>
+              <td>233</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Quality Engineer</Link>
+              </td>
+              <td>4</td>
+              <td>33</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Infrastructure Engineer</Link>
+              </td>
+              <td>1</td>
+              <td>20</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Engineering Manager</Link>
+              </td>
+              <td>0</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Product Manager</Link>
+              </td>
+              <td>3</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Product Designer</Link>
+              </td>
+              <td>6</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Technology Leadership</Link>
+              </td>
+              <td>4</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={"3"}>Engineering Leadership</Link>
+              </td>
+              <td>4</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </Table>
+      </ScrollArea>
+    </div>
+  );
 }
 
 export default RolesList;
