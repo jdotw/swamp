61bc94217e72c60a55f184693a5c4fe1ab16d1a1
James Wilson <jw@jameswilson.io>
2023-02-19 09:37:10 +1100

    Expose DbContext as Context

--- bases/Repository/RepositoryBase.cs
+++ bases/Repository/RepositoryBase.cs
@@ -7,21 +7,21 @@ namespace Base.Repository;
 
 public abstract class RepositoryBase<TEntity> : IRepository<TEntity> where TEntity : EntityBase
 {
-  private readonly DbContext _context;
+  public readonly DbContext Context;
 
   public RepositoryBase(DbContext repositoryContext)
   {
-    _context = repositoryContext;
+    Context = repositoryContext;
   }
 
   public IQueryable<TEntity> FindAllAsync()
   {
-    return _context.Set<TEntity>().AsNoTracking();
+    return Context.Set<TEntity>().AsNoTracking();
   }
 
   public virtual IQueryable<TEntity> FindByConditionAsync(Expression<Func<TEntity, bool>> expression)
   {
-    return _context.Set<TEntity>().Where(expression);
+    return Context.Set<TEntity>().Where(expression);
   }
 
   public virtual async Task<TEntity?> FindById(int id)
@@ -33,25 +33,25 @@ public abstract class RepositoryBase<TEntity> : IRepository<TEntity> where TEnti
 
   public virtual async Task AddAsync(TEntity obj)
   {
-    await _context.Set<TEntity>().AddAsync(obj);
+    await Context.Set<TEntity>().AddAsync(obj);
   }
 
   public virtual void Update(TEntity obj)
   {
-    _context.Set<TEntity>().Update(obj);
+    Context.Set<TEntity>().Update(obj);
   }
 
   public virtual void Delete(object id)
   {
-    var entity = _context.Set<TEntity>().Find(id);
+    var entity = Context.Set<TEntity>().Find(id);
     if (entity is not null)
     {
-      _context.Set<TEntity>().Remove(entity);
+      Context.Set<TEntity>().Remove(entity);
     }
   }
 
   public async Task<int> SaveAsync()
   {
-    return await _context.SaveChangesAsync();
+    return await Context.SaveChangesAsync();
   }
 }
