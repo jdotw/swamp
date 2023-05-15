07091a026605886019664123cc0b7b00685762b1
James Wilson <jw@jameswilson.io>
2023-05-15 20:46:09 +1000

    Express: Added basic get and create for Person

--- ts/index.ts
+++ ts/index.ts
@@ -1,14 +1,42 @@
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
+import bodyParser from "body-parser";
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const prisma = new PrismaClient()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server YAY dsfasf');
+});
+
+app.get('/persons', async (req, res) => {
+  const persons = await prisma.person.findMany();
+  console.log("PERSONS: ", persons);
+  res.send(JSON.stringify(persons));
+});
+
+app.post('/persons', async (req, res) => {
+  console.log("BODY: ", req.body);
+  const person = await prisma.person.create({
+    data: {
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      middleNames: req.body.middle_names,
+      externalId: req.body.external_id,
+    }
+  });
+  console.log("PERSON: ", person);
+  res.send(JSON.stringify(person));
 });
 
 app.listen(port, () => {
